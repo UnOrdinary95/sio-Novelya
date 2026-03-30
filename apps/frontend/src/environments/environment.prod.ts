1dda58c6663ef984734282b49ddcb8a4c183e867
UnOrdinary95 <114882529+UnOrdinary95@users.noreply.github.com>
2026-03-30 21:12:06 +0200

    fix: update production API URL for local development
    - change apiUrl from production domain to localhost:3000
    - enable local frontend to connect to local backend

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.novelya.unordinary.dev',
+    // apiUrl: 'https://api.novelya.unordinary.dev',
+    apiUrl: 'http://localhost:3000', // TODO: Pour les tests en local, à remplacer par l'URL de production lors du déploiement
 };
